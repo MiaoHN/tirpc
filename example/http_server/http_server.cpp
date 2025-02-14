ceedaabad22013d54f493892ad990590abb8e22f
MiaoHN <582418227@qq.com>
2025-02-14 22:45:35 +0800

    refractor: simplify http_server

--- example/http_server/http_server.cpp
+++ example/http_server/http_server.cpp
@@ -54,26 +54,6 @@ class FileHttpServlet : public tirpc::HttpServlet {
   std::string root_dir_;
 };
 
-class QPSHttpServlet : public tirpc::HttpServlet {
- public:
-  QPSHttpServlet() = default;
-  ~QPSHttpServlet() override = default;
-
-  void HandleGet(tirpc::HttpRequest *req, tirpc::HttpResponse *res) override {
-    SetHttpCode(res, tirpc::HTTP_OK);
-    SetHttpContentType(res, "text/html;charset=utf-8");
-
-    std::stringstream ss;
-    ss << "QPSHttpServlet Echo Success!! Your id is," << req->query_maps_["id"];
-    char buf[512];
-    sprintf(buf, html, ss.str().c_str());
-    SetHttpBody(res, std::string(buf));
-    AppDebugLog(ss.str().c_str());
-  }
-
-  auto GetServletName() -> std::string override { return "QPSHttpServlet"; }
-};
-
 auto main(int argc, char *argv[]) -> int {
   // default config file
   std::string config_file = "./conf/http_server.xml";
@@ -86,7 +66,6 @@ auto main(int argc, char *argv[]) -> int {
 
   auto server = std::make_shared<tirpc::HttpServer>(tirpc::GetConfig()->GetAddr());
 
-  server->RegisterHttpServlet("/qps", std::make_shared<QPSHttpServlet>());
   server->RegisterHttpServlet("/*", std::make_shared<FileHttpServlet>("./root/"));
 
   tirpc::StartServer(server);
